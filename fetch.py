21844a55df8b0595bf3c64e152bd1d5e8f234870
Akos Toth-Mate <akos@tothmate.com>
2022-02-06 23:01:18 +0100

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from time import sleep
 
 links = []
-remaining = 10
+remaining = 30
 current_date = datetime.now()
 
 while current_date >= datetime(2020, 9, 23) and remaining >= 0:
